a65536997a4ab0c828bee6573c45638d349f3b93
ycanas <yairca3.7@gmail.com>
2022-11-29 14:58:07 -0500

    creación nueva función y cambios en estilos

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
-const button = document.getElementById('button');
+const buttonBinary = document.getElementById('buttonBinary');
+const buttonText = document.getElementById('buttonText');
 
-button.onclick = function(){
+buttonBinary.onclick = function(){
     const divOutput = document.getElementById('divOutput');
-    const input = document.getElementById('input').value;
+    const input = document.getElementById('inputText').value;
     let output = "";
+
+    if(input == ''){
+        divOutput.innerHTML = "";
+        document.getElementById('divOutput').className = "";
+        return;
+    }
 
     for(let i = 0; i < input.length; i++){
         let binary = input.charCodeAt(i).toString(2).toString();
         output += Array(8-binary.length+1).join('0')+binary+" ";
     }
 
-    document.getElementById('input').value = "";
+    document.getElementById('inputText').value = "";
+    divOutput.className = "alert alert-success mx-auto";
+    divOutput.innerHTML = `${output.trim()}`;
+}
+
+buttonText.onclick = function(){
+    const divOutput = document.getElementById('divOutput');
+    const input = document.getElementById('inputText').value.split(' ');
+    let output = "";
+
+    for(let i = 0; i < input.length; i++){
+        if(isNaN(parseInt(input[0], 2))){
+            document.getElementById('inputText').value = "";
+            divOutput.className = "alert alert-success mx-auto";
+            divOutput.innerHTML = "Tipo de texto inválido";
+            return;
+        }
+
+        if(parseInt(input[0], 2) < 32){
+            divOutput.innerHTML = "";
+            divOutput.className = "";
+            document.getElementById('inputText').value = "";
+            return;
+        }
+
+        output += String.fromCharCode(parseInt(input[i], 2));
+    }
+
+    document.getElementById('inputText').value = "";
     divOutput.className = "alert alert-success mx-auto";
     divOutput.innerHTML = `${output.trim()}`;
 }
